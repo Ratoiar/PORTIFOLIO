29c65d2015b7e9d8569b079abe02681fce7164d5
Eduardo <eduardobarrosferreira10@gmail.com>
2024-05-02 18:25:04 -0300

    oidhisad

--- scripts/efeitoCarrossel.js
+++ scripts/efeitoCarrossel.js
@@ -1,1 +1,21 @@
+const swiper = new Swiper('.swiper', {
+
+
+  breakpoints: {
+ 
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 250 
+    },
+
+    990: {
+      spaceBetween: 10,
+      slidesPerView: 2,
+    }
+  },
+  
+
+});
+
+
 
