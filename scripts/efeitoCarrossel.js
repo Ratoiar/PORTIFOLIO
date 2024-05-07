921e143e695b9ac89ffb7e4ec9f6425f63107eca
Eduardo <eduardobarrosferreira10@gmail.com>
2024-05-07 10:37:40 -0300

    A GRANDE MUDANÇA

--- scripts/efeitoCarrossel.js
+++ scripts/efeitoCarrossel.js
@@ -5,22 +5,28 @@ const swiper = new Swiper('.swiper', {
  
     0: {
      
-      spaceBetween: 10, 
+      spaceBetween: 50, 
       slidesPerView: 1, 
     },
 
-    990: {
+    550:{
+      spaceBetween: 100,
+      slidesPerView: 2,
+    },
+
+    999: {
       spaceBetween: 150,
       slidesPerView: 3.4, 
       edgeSwipeThreshold: 10,
     }
   },
 
-  centeredSlides: true,
+  centeredSlides:true,
+  loop: true,
   freeMode: true,
   freeMode: {
-    enabled: true,
     sticky: true,
-    minimumVelocity: 15,
+    minimumVelocity: 100,
+    enabled: true,
   },
 });
